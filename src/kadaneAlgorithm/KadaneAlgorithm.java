8e980e4d78835b70fe64426b4159f8182dc11956
Nimish Kumar <nimishk642@gmail.com>
2024-08-09 19:21:05 +0530

    Kadane's algorithm

--- src/kadaneAlgorithm/KadaneAlgorithm.java
+++ src/kadaneAlgorithm/KadaneAlgorithm.java
@@ -2,41 +2,56 @@ package kadaneAlgorithm;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 
 public class KadaneAlgorithm {
 
-
+    // Kadane's Algorithm
     public static Integer findLargestSumOfContinuousSubArray(ArrayList<Integer> nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int max = Integer.MIN_VALUE;
         int sum = 0;
-        Integer maxSumIndex = null;
-        int maxSum = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (maxSumIndex == null) {
-                maxSumIndex = 0;
+        for (int num : nums) {
+            sum = sum + num;
+            if (sum > max) {
+                max = sum;
             }
-            sum = sum + nums.get(i);
-            map.put(i, sum);
-
-            if (map.get(maxSumIndex) < sum) {
-                maxSumIndex = i;
+            if (sum < 0) {
+                sum = 0;
             }
-        }
-        maxSum = map.get(maxSumIndex);
 
-        if (maxSumIndex == 0) {
-            return maxSum;
         }
-
-        return Collections.max(Arrays.asList(maxSum,
-                maxSum - (Collections.min(
-                        map.values().stream()
-                                .toList()
-                                .subList(0, maxSumIndex + 1)))));
+        return max;
     }
+
+    // public static Integer findLargestSumOfContinuousSubArray(ArrayList<Integer>
+    // nums) {
+    // HashMap<Integer, Integer> map = new HashMap<>();
+    // int sum = 0;
+    // Integer maxSumIndex = null;
+    // int maxSum = 0;
+
+    // for (int i = 0; i < nums.size(); i++) {
+    // if (maxSumIndex == null) {
+    // maxSumIndex = 0;
+    // }
+    // sum = sum + nums.get(i);
+    // map.put(i, sum);
+
+    // if (map.get(maxSumIndex) < sum) {
+    // maxSumIndex = i;
+    // }
+    // }
+    // maxSum = map.get(maxSumIndex);
+
+    // if (maxSumIndex == 0) {
+    // return maxSum;
+    // }
+
+    // return Collections.max(Arrays.asList(maxSum,
+    // maxSum - (Collections.min(
+    // map.values().stream()
+    // .toList()
+    // .subList(0, maxSumIndex + 1)))));
+    // }
 
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>();
